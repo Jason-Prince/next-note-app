a1dba60c87bdc0a6e08907dbc549349632062fe2
Jason Prince <jasoncprince@gmail.com>
2021-06-30 17:20:57 -0600

    front end crud is working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,45 @@
-import { GetServerSideProps, NextPage } from "next";
-import dbConnect, { jsonify } from "@/middleware/dbConnect";
-import Fighter from "@/models/Note";
+import Link from "next/link";
+import fetch from "isomorphic-unfetch";
+import { Button, Card } from "semantic-ui-react";
 
-interface Props {
-    fighters: [
-        {
-            firstName: string;
-            lastName: string;
-        }
-    ];
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-    dbConnect();
-    const fighters = await Fighter.find({}).exec();
-
-    return {
-        props: {
-            fighters: jsonify(fighters),
-        },
-    };
-};
-const Home: NextPage<Props> = ({ fighters }) => {
+const Index = ({ notes }) => {
     return (
-        <div>
-            <h1>A few fighters</h1>
-            <ul>
-                {fighters.map((fighter) => {
-                    return <li>{`${fighter.firstName} ${fighter.lastName}`}</li>;
+        <div className="notes-container">
+            <h1>Notes</h1>
+            <div className="grid wrapper">
+                {notes.map((note) => {
+                    return (
+                        <div key={note._id}>
+                            <Card>
+                                <Card.Content>
+                                    <Card.Header>
+                                        <Link href={`/${note._id}`}>
+                                            <a>{note.title}</a>
+                                        </Link>
+                                    </Card.Header>
+                                </Card.Content>
+                                <Card.Content extra>
+                                    <Link href={`/${note._id}`}>
+                                        <Button primary>View</Button>
+                                    </Link>
+                                    <Link href={`/${note._id}/edit`}>
+                                        <Button primary>Edit</Button>
+                                    </Link>
+                                </Card.Content>
+                            </Card>
+                        </div>
+                    );
                 })}
-            </ul>
+            </div>
         </div>
     );
 };
 
-export default Home;
+Index.getInitialProps = async () => {
+    const res = await fetch("http://localhost:3000/api/notes");
+    const { data } = await res.json();
+
+    return { notes: data };
+};
+
+export default Index;
